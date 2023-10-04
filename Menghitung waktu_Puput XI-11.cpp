011b347ef7a06f59344336d7b8ea9766d049ca84
puputamalia <141219297+puputamalia@users.noreply.github.com>
2023-10-04 07:50:30 +0700

    Add files via upload

---
+++ Menghitung waktu_Puput XI-11.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    double s;
+    double v;
+    double waktu;
+    double sinA;
+
+    cin >> s;
+    cin >> v;
+    sinA = sin(s * 22 / 7 / 180);
+    waktu = 2 * (v * sinA) / 10;
+    cout <<fixed<<setprecision(3)<< waktu << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
